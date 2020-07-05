72b4b7956d1d6a2a55e3cad7ea3323a8936bfc22
haleyholden <hholden@nd.edu>
2020-07-05 14:26:10 -0400

    fixed video player

--- Projects/tik_tak/build/web/flutter_service_worker.js
+++ Projects/tik_tak/build/web/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "d2aa5bba36b25f7a5b4cd21fb9a38edd",
 "/": "d2aa5bba36b25f7a5b4cd21fb9a38edd",
-"main.dart.js": "4004e98a1817ad3ff90aad18fc105d22",
+"main.dart.js": "7233efd371131cbaf69de9fe0c1ad9b6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
